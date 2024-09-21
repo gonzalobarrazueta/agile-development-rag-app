88f98a01bd8a8a805f9a4594b6551845dd3add40
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2024-09-20 22:19:44 -0500

    fix: use api key to create SearchIndexClient

--- app/rag/azure_ai_search.py
+++ app/rag/azure_ai_search.py
@@ -3,6 +3,7 @@ import os
 import azure.identity
 import dotenv
 from azure.search.documents.indexes import SearchIndexClient
+from azure.core.credentials import AzureKeyCredential
 
 dotenv.load_dotenv()
 
@@ -11,8 +12,8 @@ def get_azure_search_client():
 
     AZURE_SEARCH_SERVICE = os.getenv("AZURE_SEARCH_SERVICE")
     AZURE_SEARCH_ENDPOINT = f"https://{AZURE_SEARCH_SERVICE}.search.windows.net"
+    AZURE_SEARCH_API_KEY = os.getenv("AZURE_SEARCH_API_KEY")
 
-    azure_credential = azure.identity.DefaultAzureCredential()
-    index_client = SearchIndexClient(endpoint=AZURE_SEARCH_ENDPOINT, credential=azure_credential)
+    index_client = SearchIndexClient(endpoint=AZURE_SEARCH_ENDPOINT, credential=AzureKeyCredential(AZURE_SEARCH_API_KEY))
 
     return index_client
