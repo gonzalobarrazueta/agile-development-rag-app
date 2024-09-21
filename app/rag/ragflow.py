f6dfd563c43c5aedfdaac5d9bf0f6644cb33e894
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2024-09-21 12:33:59 -0500

    feat: integrate Azure OpenAI and Azure Search for document retrieval and chat

---
+++ app/rag/ragflow.py
@@ -0,0 +1,27 @@
+from azure_open_ai import get_embedding, get_completions
+from azure_ai_search import get_matching_documents
+
+
+def user_question(question):
+    return get_embedding(question)
+
+
+def get_sources(question):
+
+    question_embedding = user_question(question)
+    sources = get_matching_documents(question, question_embedding)
+
+    return sources
+
+
+def set_system_message(system_message):
+    return system_message
+
+
+def chat(system_message, user_question):
+    SYSTEM_MESSAGE = system_message
+    USER_MESSAGE = user_question + "\nSources: " + get_sources(user_question)
+
+    response = get_completions(SYSTEM_MESSAGE, USER_MESSAGE)
+
+    return response
