64bd5bd0e6e440643fbb7ecfe4b305ebf505c28e
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2024-09-20 22:20:46 -0500

    feat: define function to create index

--- app/rag/azure_ai_search.py
+++ app/rag/azure_ai_search.py
@@ -1,9 +1,18 @@
 import os
 
-import azure.identity
 import dotenv
 from azure.search.documents.indexes import SearchIndexClient
 from azure.core.credentials import AzureKeyCredential
+
+from azure.search.documents.indexes.models import (
+    HnswAlgorithmConfiguration,
+    SearchField,
+    SearchFieldDataType,
+    SearchIndex,
+    SimpleField,
+    VectorSearch,
+    VectorSearchProfile,
+)
 
 dotenv.load_dotenv()
 
@@ -17,3 +26,27 @@ def get_azure_search_client():
     index_client = SearchIndexClient(endpoint=AZURE_SEARCH_ENDPOINT, credential=AzureKeyCredential(AZURE_SEARCH_API_KEY))
 
     return index_client
+
+
+def create_index():
+
+    index_client = get_azure_search_client()
+    AZURE_SEARCH_INDEX = os.getenv("AZURE_SEARCH_INDEX")
+
+    index = SearchIndex(
+        name=AZURE_SEARCH_INDEX,
+        fields=[
+            SimpleField(name="id", type=SearchFieldDataType.String, key=True),
+            SearchField(name="content_vector", type=SearchFieldDataType.Collection(SearchFieldDataType.Single),
+                        searchable=True, vector_search_dimensions=1536, vector_search_profile_name="my-vector-config"),
+        ],
+        vector_search=VectorSearch(
+            profiles=[
+                VectorSearchProfile(name="my-vector-config", algorithm_configuration_name="my-algorithms-config")],
+            algorithms=[HnswAlgorithmConfiguration(name="my-algorithms-config")],
+        )
+    )
+
+    index_client.create_index(index)
+
+create_index()
